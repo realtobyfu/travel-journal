57eb74a2762ea8cd70ed0de6272e22d79e95902a
realtobyfu <fuz.sh0710@gmail.com>
2024-05-29 00:35:13 -0400

    Updated props

--- vite-react/public/data.jsx
+++ vite-react/public/data.jsx
@@ -6,7 +6,7 @@ export default [
         startDate: "12 Jan, 2021",
         endDate: "24 Jan, 2021",
         description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-        imageUrl: "https://images.unsplash.com/photo-1549180030-48bf079fb38a?q=80&w=2971&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        imageUrl: "https://images.unsplash.com/photo-1525635569544-610f9540664a?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
     },
     {
         title: "Sydney Opera House",
--- vite-react/src/App.jsx
+++ vite-react/src/App.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
-import data from "../public/data";
+import data from '../public/data';
 import Navbar from "../components/Navbar.jsx"
 import Travel from "../components/Travel.jsx"
 import './App.css'
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
 
+    const travels = data.map(item => {
+        return (
+            <Travel
+                key = {uuidv4()}
+                {...item}
+            />
+        )
+    })
+
+
     return (
         <div className="App">
-            <Navbar />
-            <Travel />
+                <Navbar />
+                {travels}
         </div>
     );
 }
